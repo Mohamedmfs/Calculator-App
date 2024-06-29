4564f5b329e35b3efbdf7ab0251439e2f7ab1c26
Mohamedmfs <129542529+Mohamedmfs@users.noreply.github.com>
2024-06-29 12:29:14 -0400

    Updated it a bit.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const display = document.querySelector('#display');
 const operators = document.querySelectorAll('.operator');
-const buttons = document.querySelectorAll('#btn');
+const numbers = document.querySelectorAll('#number');
 
 // operators
 operators.forEach((operator) => {
@@ -12,7 +12,7 @@ operators.forEach((operator) => {
       display.value = display.value.slice(0, -1);
     }
     else if (e.target.value === '+' || e.target.value === '-' || e.target.value === '*' || e.target.value === '/' || e.target.value === '.') {
-      display.value = display.value + e.target.value
+      display.value += e.target.value
     }
     else if (e.target.value === '=') {
       display.value = eval(display.value);
@@ -21,10 +21,10 @@ operators.forEach((operator) => {
   operator.addEventListener('click', calculate);
 })
 
-// buttons
-buttons.forEach((button) => {
+// numbers
+numbers.forEach((number) => {
   const show = (e) => {
     display.value += e.target.value;
   }
-  button.addEventListener('click', show);
+  number.addEventListener('click', show);
 })
